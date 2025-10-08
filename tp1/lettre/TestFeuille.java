29908202275899202239a07987aaf0f33b2d2e71
1 mec <77551635+1-mec@users.noreply.github.com>
2025-10-08 17:34:08 -0400

    Add files via upload

---
+++ tp1/lettre/TestFeuille.java
@@ -0,0 +1,27 @@
+package papier;
+
+public class TestFeuille {
+
+	public static void main(String[] args) {
+		Feuille f = new Feuille();
+		f.setVerso("oui oui baguette");
+		f.setRecto("et non mdrr");
+		f.retourner();
+		f.aff();
+		f.retourner();
+		f.aff();	
+		f.retourner();
+		f.aff();
+		f.retourner();
+		System.out.println("");
+		f.setVerso("blablabla");
+		f.setRecto("bruh");
+		f.retourner();
+		f.aff();
+		f.retourner();
+		f.aff();
+		f.retourner();
+		f.aff();
+	}
+
+}
