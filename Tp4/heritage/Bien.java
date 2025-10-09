724485f50a1bae889229e3509787533556cefc99
1 mec <77551635+1-mec@users.noreply.github.com>
2025-10-09 10:02:08 +0200

    Add files via upload

--- Tp4/heritage/Bien.java
+++ Tp4/heritage/Bien.java
@@ -1,1 +1,49 @@
+package gestionVille;
 
+public class Bien {
+	private int valEuro;
+	private int CountEntretientMensuel;
+	private static int nbExemple;
+	
+	public Bien() {
+		valEuro = 0;
+		CountEntretientMensuel = 0;
+	}
+
+	public Bien(int valEuro, int countEntretientMensuel) {
+		super();
+		this.valEuro = valEuro;
+		CountEntretientMensuel = countEntretientMensuel;
+	}
+
+	public int getValEuro() {
+		return valEuro;
+	}
+
+	public void setValEuro(int valEuro) {
+		this.valEuro = valEuro;
+	}
+
+	public int getCountEntretientMensuel() {
+		return CountEntretientMensuel;
+	}
+
+	public void setCountEntretientMensuel(int countEntretientMensuel) {
+		CountEntretientMensuel = countEntretientMensuel;
+	}
+
+	public static int getNbExemple() {
+		return nbExemple;
+	}
+
+	@Override
+	public String toString() {
+		return "Bien [valEuro=" + valEuro + ", CountEntretientMensuel=" + CountEntretientMensuel + "]";
+	}
+	
+	public String getInfo() {
+		return " Bien [ valEuro :" + this.getValEuro() +  " , CountEntretientMensuel :" + this.getCountEntretientMensuel() +" ]";
+	}
+	
+	
+}
