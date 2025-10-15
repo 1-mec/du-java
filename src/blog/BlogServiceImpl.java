72e3db7b5d3fb0982ca4e80b8e48b147c6d6a5e9
1-mec <matteojonas3@gmail.com>
2025-10-15 23:25:07 +0200

    BlogServiceImpl

--- src/blog/BlogServiceImpl.java
+++ src/blog/BlogServiceImpl.java
@@ -57,49 +57,98 @@ public class BlogServiceImpl implements BlogService{
 	
 	public List<Publishable> findItemsByAuthor(String author) {
 		List<Publishable> p = new ArrayList<Publishable>();
-		for (int i = 0; i < this.items.size(); i ++) {
-			if (((Publishable) this.items).getAuthor() == author) {
-				p.add(i, this.items.get(i));
+		if (author != null) {
+			for (int i = 0; i < this.items.size(); i ++) {
+				if (( this.items.get(i)).getAuthor().equals(author) ) {
+					p.add(this.items.get(i));
+				}
 			}
 		}
-		this.items = p;
-		return this.items;
+		return p;
 	}
 	
 	public Publishable getLatestItem() {
-		Publishable a = ((Publishable)this.items.get(0));
-		for (int i = 1 ; i < this.items.size();i++) {
-			if (((Publishable) this.items.get(i)).getPublicationDate() >((Publishable) this.items.get(i-1)).getPublicationDate()) {
-				a = this.items.get(i);
-			}
-		}
-		return a ;
+	    if (items.isEmpty()) {
+	        return null;
+	    }
+
+	    Publishable latest = items.get(0);
+	    for (int i = 1; i < this.items.size(); i++) {
+	        if (this.items.get(i).getPublicationDate() > latest.getPublicationDate()) {
+	            latest = items.get(i);
+	        }
+	    }
+	    return latest;
 	}
+
+
 
 	
 	public List<Publishable> findItemsByTags(String[] tags) {
-		List<Publishable> p = new ArrayList<Publishable>();
-		for (int i = 0; i < this.items.size(); i ++) {
-			if (tags[i].equals(p.get(i))) {
-				p.add(i, this.items.get(i));
-			}
-		}
-		this.items = p;
-		return this.items;
+	    List<Publishable> result = new ArrayList<>();
+	    if (tags == null || tags.length == 0) return result;
+
+	    for (int i = 0 ; i < this.items.size() ; i++) {
+	        if (this.items.get(i) instanceof Taggable) {
+	            List<String> Tags = ((Taggable) this.items.get(i)).getTags();
+	            int y = 0;
+	            boolean tout = true;
+
+	            while (y < tags.length && tout) {
+	                if (!Tags.contains(tags[y])) {
+	                	tout = false; 
+	                }
+	                y++;
+	            }
+
+	            if (tout) result.add(this.items.get(i));
+	        }
+	    }
+
+	    return result;
 	}
+
 	
 	public List<Publishable> findItemsByContent(String[] words) {
-		return null;
+	    List<Publishable> result = new ArrayList<>();
+	    if (words == null || words.length == 0) return result;
+
+	    for (int i = 0 ; i < this.items.size() ; i++) {
+	        if (this.items.get(i) instanceof Message) {
+	            String msg = (((Message) this.items.get(i)).getContent());
+	            int y = 0;
+	            boolean tout = true;
+
+	            while (y < words.length && tout) {
+	                if (msg.indexOf(words[y]) == -1) {
+	                	tout = false; 
+	                }
+	                y++;
+	            }
+
+	            if (tout) result.add(this.items.get(i));
+	        }
+	    }
+
+	    return result;
 	}
 		
 	public List<Publishable> findItemsByTagsOrContent(String[] tags) {
-		List<Publishable> p = new ArrayList<Publishable>();
-		for (int i = 0; i < this.items.size(); i ++) {
-			if (tags[i].equals(p.get(i))) {
-				p.add(i, this.items.get(i));
-			}
-		}
-		return p ;
+	    List<Publishable> result = new ArrayList<>();
+	    
+	    List<Publishable> tag = findItemsByTags(tags);
+	    for (int i = 0; i < tag.size(); i++) {
+	        result.add(tag.get(i));
+	    }
+	    
+	    List<Publishable> cont = findItemsByContent(tags);
+	    for (int i = 0; i < cont.size(); i++) {
+	        result.add(cont.get(i));
+	    }
+	    
+	    return result;
 	}
+
 	
 }
+
