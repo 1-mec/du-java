6e9ecf96b21a848f3182dccdf8394602a96a7eb6
1 mec <77551635+1-mec@users.noreply.github.com>
2025-11-25 17:47:02 +0100

    Update DAO.java

--- Dao/LeDao/DAO.java
+++ Dao/LeDao/DAO.java
@@ -1,6 +1,8 @@
 package DAO;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 
@@ -8,8 +10,18 @@ public abstract class DAO<T> {
 	protected Connection connect ;
 	protected Statement stmt;
 	
+	public DAO() {
+		open();
+	}
+	
 	public void open() {
-		SingleConnection.getInstance();
+		connect = SingleConnection.getInstance();
+		try {
+			stmt = connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public abstract T create (T obj) ;
